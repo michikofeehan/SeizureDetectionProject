edf94e69f851c1a641ba24c7fb8ca493e48e4844
Michiko Feehan <michiko.feehan@gmail.com>
2018-03-31 02:31:13 -0400

    Added return statements

--- DetectSeizure.java
+++ DetectSeizure.java
@@ -1,18 +1,31 @@
 import java.util.Scanner;
-public class DetectSeizure {
-	
+public class Detection {
 	
 	public static void main(String[] args) {
 	boolean seizure = false; // initially set as no seizure
 	double threshold = 20; // the absolute difference between timed average and peak
 	double difference = 0;
+	double currentConductanceValue = 0;
+	
 		//Keep looping until threshold is greater than 20uS and a seizure is detected
-		while (seizure == false && difference < threshold){ // redundant here but keep maybe
+		while (difference < threshold){ // redundant here but keep maybe
 		Scanner scanner = new Scanner(System.in);
-		double currentConductanceValue = scanner.nextDouble();// input most recent current conductance value	
+		
+		System.out.println("\n Please enter current conductance numerical value (uS) \n");
+		currentConductanceValue = scanner.nextDouble();// input most recent current conductance value	
+		
+		System.out.println("\n Please enter current baseline value numerical value (uS) \n");
 		double baseline = scanner.nextDouble(); // timed average input
+		
 		difference = Math.abs( baseline - currentConductanceValue );// redefine threshold
 	}
+		if (difference <20){
+		System.out.println("\nThe difference is " + difference + " uS. You do not have a grand mal seizure! \n");
+		}
+		else{
+		System.out.println("\nThe difference is " + difference + "\n\nYou have a grand mal seizure!!! Your caretakers will be alerted!!!!\n");
+		seizure = true; 
+		}
 	
 	}
 
